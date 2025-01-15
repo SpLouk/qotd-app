fe29a4ba921edd8390cb1d92c206012d6f2a01de
David Loukidelis <david@loukidelis.com>
2025-01-14 22:22:59 -0500

    add users and sessions

--- backend/db/migrate/20250114193630_create_users.rb
+++ backend/db/migrate/20250114193630_create_users.rb
@@ -1,11 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :email_address, null: false
-      t.string :password_digest, null: false
+      t.string :user_id
+      t.string :email_address
+      t.string :password_digest
+      t.string :username
 
       t.timestamps
     end
     add_index :users, :email_address, unique: true
+    add_index :users, :user_id, unique: true
   end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -22,11 +22,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_14_193631) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email_address", null: false
-    t.string "password_digest", null: false
+    t.string "user_id"
+    t.string "email_address"
+    t.string "password_digest"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["user_id"], name: "index_users_on_user_id", unique: true
   end
 
   add_foreign_key "sessions", "users"
