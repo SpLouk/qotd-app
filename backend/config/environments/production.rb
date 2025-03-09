c9943c7a3a4af514a374244d0f096c7c56f942b0
David Loukidelis <david@loukidelis.com>
2025-03-09 11:45:10 -0400

    configure active storage prod

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -12,6 +12,11 @@ Rails.application.configure do
   # Full error reports are disabled.
   config.consider_all_requests_local = false
 
+  # Set default URL options for Active Storage
+  Rails.application.routes.default_url_options = {
+    host: "loukidelis.ca"
+  }
+
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
