bb2073fcfe4169ed3561c29767ec760852bf415f
David Loukidelis <david@loukidelis.com>
2025-03-16 20:59:57 -0400

    Add notifications for post comments and prompt activation

--- backend/app/jobs/activate_prompt_question_job.rb
+++ backend/app/jobs/activate_prompt_question_job.rb
@@ -12,6 +12,15 @@ class ActivatePromptQuestionJob < ApplicationJob
     prompt.activate!
 
     # Send push notifications to all users
-    ApnsService.notify_new_prompt(prompt)
+    device_tokens = DeviceToken.all
+
+    notification = Notification.new(
+      title: "Time to write",
+      body: "A new question is active. Respond to it now!",
+      category: "new_prompt",
+      thread_id: "prompt_#{prompt.id}",
+      target_content_id: prompt.id.to_s
+    )
+    ApnsService.notify(notification, device_tokens)
   end
 end
--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -6,10 +6,40 @@ class Post < ApplicationRecord
 
   validates :content, presence: true
 
+  after_create :notify_parent_post_author, if: :is_reply?
+
   def as_json
     attrs = super
     attrs[:username] = user.username
     attrs[:user_photo_url] = user.profile_photo.attached? ? Rails.application.routes.url_helpers.rails_blob_url(user.profile_photo) : nil
     attrs
+  end
+
+  private
+
+  def is_reply?
+    parent_post.present?
+  end
+
+  def notify_parent_post_author
+    return unless parent_post.user.device_tokens.any?
+
+    notification = Notification.new(
+      title: "#{user.username} responded to your post",
+      body: content,
+      category: "post_response",
+      thread_id: "post_#{parent_post.id}",
+      target_content_id: parent_post.id.to_s,
+      custom_data: {
+        responder_id: user.id,
+        responder_username: user.username,
+        responder_profile_photo_url: user.profile_photo.attached? ? Rails.application.routes.url_helpers.rails_blob_url(user.profile_photo) : nil,
+        post_id: parent_post.id,
+        response_id: id,
+        response_content: content
+      }
+    )
+
+    ApnsService.notify(notification, parent_post.user.device_tokens)
   end
 end
