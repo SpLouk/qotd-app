f0b2a0723d34f77e3b1027260b675ecdba97a57d
David Loukidelis <david@loukidelis.com>
2025-06-04 10:28:00 -0400

    update port smtp

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
     user_name: Rails.application.credentials.dig(:smtp, :user_name),
     password: Rails.application.credentials.dig(:smtp, :password),
     address: "smtp.sendgrid.net",
-    port: 587,
+    port: 2525,
     authentication: :plain,
     enable_starttls_auto: true,
     domain: "loukidelis.com"
