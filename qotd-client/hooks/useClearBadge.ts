944cfd4cf471909ee31408763a6f4056bbd598b6
David Loukidelis <david@loukidelis.com>
2025-06-27 14:48:19 -0400

    Fix badge clearing

--- qotd-client/hooks/useClearBadge.ts
+++ qotd-client/hooks/useClearBadge.ts
@@ -1,8 +1,24 @@
 import { setBadgeCountAsync } from 'expo-notifications';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import { AppState } from 'react-native';
 
+/**
+ * Based on snippet from https://reactnative.dev/docs/appstate.
+ * We want to reset the notification badge whenever the app is brought to the foreground
+ */
 export const useClearBadge = () => {
+  const appState = useRef(AppState.currentState);
   useEffect(() => {
-    setBadgeCountAsync(0);
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      if (appState.current.match(/inactive|background/) && nextAppState === 'active') {
+        setBadgeCountAsync(0);
+      }
+
+      appState.current = nextAppState;
+    });
+
+    return () => {
+      subscription.remove();
+    };
   }, []);
 };
