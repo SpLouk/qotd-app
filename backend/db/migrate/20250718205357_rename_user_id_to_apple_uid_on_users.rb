d6d551e34d476bf134a474f9754edf1901deda58
David Loukidelis <david@loukidelis.com>
2025-07-22 18:00:18 -0400

    fixup db migration

--- backend/db/migrate/20250718205357_rename_user_id_to_apple_uid_on_users.rb
+++ backend/db/migrate/20250718205357_rename_user_id_to_apple_uid_on_users.rb
@@ -1,6 +1,5 @@
 class RenameUserIdToAppleUidOnUsers < ActiveRecord::Migration[8.0]
   def change
     rename_column :users, :user_id, :apple_uid
-    add_index :users, :apple_uid, unique: true
   end
 end
